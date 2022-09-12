725f45c759862ffa8b0a0f00763f0e234964e153
Mati <maticzacza@gmail.com>
2022-09-12 15:23:28 +0300

    lab12 ui done

--- Labs/Lab12 - Bluetooth Beacon/app/src/main/java/com/czaacza/bluetoothbeaconproject/MainActivity.kt
+++ Labs/Lab12 - Bluetooth Beacon/app/src/main/java/com/czaacza/bluetoothbeaconproject/MainActivity.kt
@@ -3,6 +3,18 @@ package com.czaacza.bluetoothbeaconproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.czaacza.bluetoothbeaconproject.ui.theme.BluetoothBeaconProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +22,43 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BluetoothBeaconProjectTheme {
+                ShowContent()
+            }
+        }
+    }
+}
 
+@Composable
+fun ShowContent() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(40.dp)
+    ) {
+        Button(
+            onClick = { /*TODO*/ },
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier
+                .width(250.dp)
+                .height(60.dp)
+        ) {
+            Text(text = "Start scanning", fontSize = 22.sp)
+        }
+        Spacer(modifier = Modifier.size(60.dp))
+        
+        Text(
+            text = "Bluetooth device list:",
+            fontSize = 20.sp,
+            fontWeight = FontWeight(600)
+        )
+        
+        LazyColumn(
+            horizontalAlignment = Alignment.Start,
+            modifier = Modifier.padding(top = 20.dp).fillMaxSize()
+        ) {
+            item {
+                Text(text = "sample item text", fontSize = 18.sp)
             }
         }
     }
